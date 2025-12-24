e3683fef31f7a56017100b5624ca85ee28859caa
Olga Baynova (terra456) <terra.456@yandex.ru>
2025-12-25 01:01:29 +0500

    feat: change logo and add styles for chart section and it work in js

--- src/main.js
+++ src/main.js
@@ -6,14 +6,52 @@ import { createSalesChart } from "./scripts/chart-component.js";
 console.log("is started");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("salesChart");
+  const canvas = document.getElementById("chart");
+  const btn25 = document.getElementById("chart-25");
+  const btn26 = document.getElementById("chart-26");
+
+  const labels = [
+    "январь",
+    "февраль",
+    "март",
+    "апрель",
+    "май",
+    "июнь",
+    "июль",
+    "август",
+    "сентябрь",
+    "октябрь",
+    "ноябрь",
+    "декабрь",
+  ];
+  const values25 = [0, 0, 0, 0, 0, 0, 10000, 15000, 12000, 18000, 16000, 20000];
+  const values26 = [10000, 15000, 12000, 18000, 16000, 20000, 0, 0, 0, 0, 0, 0];
 
   if (canvas) {
-    const chart = createSalesChart("salesChart", {
-      labels: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн"],
-      values: [10000, 15000, 12000, 18000, 16000, 20000],
+    let chart = createSalesChart("salesChart", {
+      labels,
+      values: values25,
     });
     console.log("Chart created:", chart);
+
+    btn25.addEventListener("click", () => {
+      btn26.classList.remove("active");
+      btn25.classList.add("active");
+      chart.destroy();
+      chart = createSalesChart("salesChart", {
+        labels,
+        values: values25,
+      });
+    });
+    btn26.addEventListener("click", () => {
+      btn25.classList.remove("active");
+      btn26.classList.add("active");
+      chart.destroy();
+      chart = createSalesChart("salesChart", {
+        labels,
+        values: values26,
+      });
+    });
   } else {
     console.warn("Canvas element #salesChart not found");
   }
--- src/scripts/chart-component.js
+++ src/scripts/chart-component.js
@@ -29,8 +29,8 @@ export function createSalesChart(canvasId, data) {
 
   // Создаем градиент
   const gradient = ctx.createLinearGradient(0, 0, 0, 400);
-  gradient.addColorStop(0, "rgba(37, 99, 235, 0.3)");
-  gradient.addColorStop(1, "rgba(37, 99, 235, 0)");
+  gradient.addColorStop(0, "rgba(25, 86, 205, 0.5)");
+  gradient.addColorStop(1, "rgba(133, 167, 235, 0.3)");
 
   return new Chart(ctx, {
     type: "line",
@@ -38,17 +38,15 @@ export function createSalesChart(canvasId, data) {
       labels: data.labels,
       datasets: [
         {
-          label: "Продажи",
+          label: "Экономия",
           data: data.values,
-          borderColor: "#2563eb",
+          borderColor: "#2e6ce6",
           backgroundColor: gradient,
-          borderWidth: 3,
+          borderWidth: 2,
           fill: true,
           tension: 0.4,
-          pointBackgroundColor: "#2563eb",
-          pointBorderColor: "#ffffff",
-          pointBorderWidth: 2,
-          pointRadius: 5,
+          //pointBackgroundColor: '#2563eb',
+          pointStyle: false,
         },
       ],
     },
